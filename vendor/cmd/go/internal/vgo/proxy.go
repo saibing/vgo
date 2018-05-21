63b77f49497f9964b2cdc626b3453baeb38c50f2
dengsaibing 00257414 <dengsaibing@huawei.com>
2018-05-21 21:28:19 +0800

    add proxy feature

--- vendor/cmd/go/internal/vgo/proxy.go
+++ vendor/cmd/go/internal/vgo/proxy.go
@@ -2,11 +2,10 @@ package vgo
 
 import (
 	"fmt"
-	
+
 	"cmd/go/internal/modfetch"
 	"cmd/go/internal/module"
 )
-
 
 // Fetch returns the directory in the local download cache
 // holding the root of mod's source tree.
@@ -17,7 +16,14 @@ func Fetch(path string, version string) (dir string, err error) {
 		return "", err
 	}
 
+	reqs := newReqs()
+
 	mod := module.Version{Path: path, Version: info.Version}
+	list, err := reqs.Required(mod)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("list:", list)
 	fmt.Printf("fetch module %s %s\n", path, info.Version)
 	fmt.Println(info)
 	return fetch(mod)
