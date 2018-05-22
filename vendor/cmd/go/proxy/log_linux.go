0c5d8d5d7644c66a026ab645a6e3f4e5633e4647
dengsaibing 00257414 <dengsaibing@huawei.com>
2018-05-22 20:47:18 +0800

    support color output

--- vendor/cmd/go/proxy/log_linux.go
+++ vendor/cmd/go/proxy/log_linux.go
@@ -20,5 +20,5 @@ func logInfo(format string, a ...interface{}) {
 func logError(format string, a ...interface{}) {
 	now := time.Now().Format("0102 15:04:05.999")
 	msg := fmt.Sprintf(format, a...)
-	fmt.Printf("%c[1;40;31m%s%c[0m\n", 0x1B, now+""+msg, 0x1B)
+	fmt.Printf("%c[1;40;31m%s%c[0m\n", 0x1B, now+" "+msg, 0x1B)
 }
